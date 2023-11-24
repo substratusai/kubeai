33090a38770c494771436f584475f3e443cbe560
Sam Stoelinga <sammiestoel@gmail.com>
2023-11-23 20:13:45 -0800

    add test for worker

--- tests/batch_inference/main_test.py
+++ tests/batch_inference/main_test.py
@@ -1,12 +1,16 @@
 import asyncio
 import pathlib
 import json
-import pytest
 import os
 import unittest
 from unittest.mock import patch, mock_open
 
-from .main import flusher
+import aiohttp
+import pytest
+from pytest_httpserver import HTTPServer
+
+from .main import flusher, worker
+
 
 @pytest.mark.asyncio
 async def test_batching_and_flushing(tmp_path: pathlib.Path):
@@ -30,20 +34,29 @@ async def test_batching_and_flushing(tmp_path: pathlib.Path):
     assert total_lines == 25, f"Expected 25 lines, got {total_lines}."
 
 
-
 @pytest.mark.asyncio
 async def test_termination_condition():
     results = asyncio.Queue(maxsize=100)
     await results.put(None)  # Signal to end the loop
     await flusher(results, flush_every=10, output_path="")
 
+
 @pytest.mark.asyncio
-async def test_worker():
+async def test_worker(httpserver: HTTPServer):
+    dummy_data = {"foo": "bar"}
+    dummy_request = {"title": "result_1"}
+    httpserver.expect_request("/v1/completions").respond_with_json(dummy_data)
+    url = httpserver.url_for("/v1/completions")
     requests = asyncio.Queue(maxsize=100)
     results = asyncio.Queue(maxsize=100)
     worker_id = 1
-    url = "http://localhost:8080/v1/inference"
     session = aiohttp.ClientSession()
-    await requests.put({"title": "result_1"})
+    await requests.put(dummy_request)
     await requests.put(None)
-
+    task = asyncio.create_task(worker(requests, results, session, worker_id, url))
+    async with asyncio.timeout(10):
+        await requests.join()
+    assert results.qsize() == 1
+    result = await results.get()
+    assert result == {"request": dummy_request, "response": dummy_data}
+    session.close()
