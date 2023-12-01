aba3e64ca98e4e3f8a0a89efdbe26033c1d93d1a
Nick Stogner <nicholas.stogner@gmail.com>
2023-11-30 22:09:21 -0500

    Add cleanup to test

--- tests/performance/test.sh
+++ tests/performance/test.sh
@@ -8,6 +8,12 @@ this_dir=$(dirname "$0")
 
 # Deploy lingo.
 skaffold run
+function cleanup {
+	skaffold delete
+	kubectl delete -f $this_dir/backend-deployment.yaml
+	kubectl delete -f $this_dir/backend-service.yaml
+}
+trap cleanup EXIT
 
 # Make sure backend is deployed and scaled to zero.
 kubectl apply -f $this_dir/backend-service.yaml
@@ -25,3 +31,4 @@ kubectl create -f $this_dir/test-pod.yaml
 kubectl wait -f $this_dir/test-pod.yaml --for=condition=ready --timeout=30s
 kubectl logs -f perf-test --tail=-1 --follow
 kubectl wait -f $this_dir/test-pod.yaml --for=condition=complete --timeout=6s
+
