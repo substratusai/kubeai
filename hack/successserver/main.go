b9f7d64c0804daeae8adeb867380e716662b8db2
Nick Stogner <nicholas.stogner@gmail.com>
2024-01-18 12:37:23 -0500

    Copy request body

--- hack/successserver/main.go
+++ hack/successserver/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+	"os"
+)
 
 func main() {
 	http.ListenAndServe(":8082", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.Copy(os.Stdout, r.Body)
 		w.Write([]byte("success\n"))
 	}))
 }
