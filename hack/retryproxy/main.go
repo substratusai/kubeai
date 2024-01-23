875da1133bf4f7bc2ce5f7b7a4834208b0dcc1a5
Nick Stogner <nicholas.stogner@gmail.com>
2024-01-23 10:34:46 -0500

    Add retries to handler

--- hack/retryproxy/main.go
+++ hack/retryproxy/main.go
@@ -12,6 +12,8 @@ import (
 )
 
 func main() {
+	var maxRetries = 1
+
 	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println("serving")
 
@@ -23,14 +25,14 @@ func main() {
 
 		fmt.Println("body:", string(body))
 
-		newProxy(body, 0).ServeHTTP(w, newRequest(r, body))
+		newProxy(body, 0, maxRetries).ServeHTTP(w, newRequest(r, body))
 	}))
 
 }
 
 var errRetry = errors.New("retry")
 
-func newProxy(body []byte, attempt int) http.Handler {
+func newProxy(body []byte, attempt, maxRetries int) http.Handler {
 	// go run ./hack/failserver
 	u, err := url.Parse(getEndpoint(attempt))
 	if err != nil {
@@ -47,14 +49,17 @@ func newProxy(body []byte, attempt int) http.Handler {
 	}
 
 	proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
-		if err == errRetry {
+		if err != nil && attempt < maxRetries {
 			log.Println("retrying")
 
 			// Simulate calling the next backend.
 			// go run ./hack/successserver
-			newProxy(body, attempt+1).ServeHTTP(w, newRequest(r, body))
+			newProxy(body, attempt+1, maxRetries).ServeHTTP(w, newRequest(r, body))
 			return
 		}
+
+		log.Printf("http: proxy error: %v", err)
+		w.WriteHeader(http.StatusBadGateway)
 	}
 
 	return proxy
---
+++ pkg/proxy/request.go
@@ -0,0 +1,113 @@
+package proxy
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/google/uuid"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// proxyRequest keeps track of the state of a request that is to be proxied.
+type proxyRequest struct {
+	// r is the original request. It is stored here so that is can be cloned
+	// and sent to the backend while preserving the original request body.
+	r *http.Request
+	// body will be stored here if the request body needed to be read
+	// in order to determine the model.
+	body []byte
+
+	// metadata:
+
+	id                string
+	status            int
+	model             string
+	backendDeployment string
+	attempt           int
+
+	// metrics:
+
+	timer *prometheus.Timer
+}
+
+func newProxyRequest(r *http.Request) *proxyRequest {
+	pr := &proxyRequest{
+		r:      r,
+		id:     uuid.New().String(),
+		status: http.StatusOK,
+	}
+
+	pr.timer = prometheus.NewTimer(prometheus.ObserverFunc(func(v float64) {
+		httpDuration.WithLabelValues(pr.model, strconv.Itoa(pr.status)).Observe(v)
+	}))
+
+	return pr
+
+}
+
+func (p *proxyRequest) done() {
+	p.timer.ObserveDuration()
+}
+
+func (pr *proxyRequest) parseModel() error {
+	pr.model = pr.r.Header.Get("X-Model")
+	if pr.model != "" {
+		return nil
+	}
+
+	var err error
+	pr.body, err = io.ReadAll(pr.r.Body)
+	if err != nil {
+		return fmt.Errorf("read: %w", err)
+	}
+
+	var payload struct {
+		Model string `json:"model"`
+	}
+	if err := json.Unmarshal(pr.body, &payload); err != nil {
+		return fmt.Errorf("unmarshal json: %w", err)
+	}
+	pr.model = payload.Model
+
+	if pr.model == "" {
+		return fmt.Errorf("no model specified")
+	}
+
+	return nil
+}
+
+func (pr *proxyRequest) sendErrorResponse(w http.ResponseWriter, status int, format string, args ...interface{}) {
+	msg := fmt.Sprintf(format, args...)
+	log.Printf("sending error response: %v: %v", status, msg)
+
+	pr.setStatus(w, status)
+
+	if status >= 500 {
+		// Don't leak internal error messages to the client.
+		msg = http.StatusText(status)
+	}
+
+	if err := json.NewEncoder(w).Encode(struct {
+		Error string `json:"error"`
+	}{
+		Error: msg,
+	}); err != nil {
+		log.Printf("error encoding error response: %v", err)
+	}
+}
+
+func (pr *proxyRequest) setStatus(w http.ResponseWriter, code int) {
+	pr.status = code
+	w.WriteHeader(code)
+}
+
+func (pr *proxyRequest) httpRequest() *http.Request {
+	clone := pr.r.Clone(pr.r.Context())
+	clone.Body = io.NopCloser(bytes.NewReader(pr.body))
+	return clone
+}
