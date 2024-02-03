9a049f13b1f24d7fc5097d4a8b6af048d0a297b1
Sam Stoelinga <sammiestoel@gmail.com>
2024-02-03 09:17:18 -0800

    increase sleep from 20 to 30

--- tests/e2e/test.sh
+++ tests/e2e/test.sh
@@ -91,9 +91,9 @@ fi
 
 # Verify that leader election works by forcing a 20 second apiserver outage
 KIND_NODE=$(kind get nodes --name=substratus-test)
-docker exec -ti ${KIND_NODE} iptables -I INPUT -p tcp --dport 6443 -j DROP
-sleep 20
-docker exec -ti ${KIND_NODE} iptables -D INPUT -p tcp --dport 6443 -j DROP
+docker exec ${KIND_NODE} iptables -I INPUT -p tcp --dport 6443 -j DROP
+sleep 30
+docker exec ${KIND_NODE} iptables -D INPUT -p tcp --dport 6443 -j DROP
 
 echo "Waiting for deployment to scale down back to 0 within 2 minutes"
 for i in {1..15}; do
