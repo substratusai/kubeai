fae4bab3de64961b221bf0fb719c90971a298a4f
Sam Stoelinga <sammiestoel@gmail.com>
2024-02-05 23:48:56 -0800

    in some cases state is incorrect so just scale to desired scale

--- pkg/deployments/scaler.go
+++ pkg/deployments/scaler.go
@@ -86,12 +86,6 @@ func (s *scaler) compareScales(current, desired int32) {
 		// Scale up immediately.
 		go s.scaleFunc(s.desiredScale, false)
 		s.scaleDownStarted = false
-	} else if s.desiredScale == s.currentScale {
-		// Do nothing, schedule nothing.
-		if s.scaleDownTimer != nil {
-			s.scaleDownTimer.Stop()
-		}
-		s.scaleDownStarted = false
 	} else {
 		// Schedule a scale down.
 
