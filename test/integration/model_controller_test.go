fdcc1c63b7c50d0ab66a655349a488eb771df868
Nick Stogner <nicholas.stogner@gmail.com>
2024-09-07 17:00:44 -0400

    Add test cases for Pod creation

--- test/integration/model_controller_test.go
+++ test/integration/model_controller_test.go
@@ -49,6 +49,13 @@ func TestModelScaling(t *testing.T) {
 		return replicas != 0
 	}, 2*time.Second, time.Second/10, "Model should not scale up yet")
 
+	// Make sure no Pods are created for the Model yet.
+	require.Never(t, func() bool {
+		podList := &corev1.PodList{}
+		require.NoError(t, testK8sClient.List(testCtx, podList, client.InNamespace(testNS), client.MatchingLabels{"model": m.Name}))
+		return len(podList.Items) != 0
+	}, 2*time.Second, time.Second/10, "No model Pods should be created yet")
+
 	require.EventuallyWithT(t, func(t *assert.CollectT) {
 		// Retrieve the Model object from the Kubernetes cluster.
 		assert.NoError(t, testK8sClient.Get(testCtx, client.ObjectKeyFromObject(&m), &m))
@@ -78,4 +85,11 @@ func TestModelScaling(t *testing.T) {
 		assert.NotNil(t, m.Spec.Replicas)
 		assert.Equal(t, int32(1), *m.Spec.Replicas)
 	}, 2*time.Second, time.Second/10, "Model should scale up to MinReplicas after update")
+
+	// Check that a Pod was created for the Model.
+	require.EventuallyWithT(t, func(t *assert.CollectT) {
+		podList := &corev1.PodList{}
+		assert.NoError(t, testK8sClient.List(testCtx, podList, client.InNamespace(testNS), client.MatchingLabels{"model": m.Name}))
+		assert.Len(t, podList.Items, 1)
+	}, 2*time.Second, time.Second/10, "Pod should be created for the Model")
 }
