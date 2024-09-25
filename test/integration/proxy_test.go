face70e45f0ba3341293ca8fd01de895749ef278
Nick Stogner <nicholas.stogner@gmail.com>
2024-09-24 20:55:07 -0400

    Extend test timeouts

--- test/integration/proxy_test.go
+++ test/integration/proxy_test.go
@@ -70,7 +70,7 @@ vllm:num_requests_waiting{model_name="%s"} %d.0
 	sendRequests(t, &wg, m.Name, 1, http.StatusOK)
 
 	requireModelReplicas(t, m, 1, "Replicas should be scaled up to 1 to process messaging request", time.Second)
-	requireModelPods(t, m, 1, "Pod should be created for the messaging request", time.Second)
+	requireModelPods(t, m, 1, "Pod should be created for the messaging request", 5*time.Second)
 	markAllModelPodsReady(t, m)
 	completeRequests(backendComplete, 1)
 	require.Equal(t, int32(1), totalBackendRequests.Load(), "ensure the request made its way to the backend")
@@ -80,7 +80,7 @@ vllm:num_requests_waiting{model_name="%s"} %d.0
 	// Simulate the backend processing the request.
 	sendRequests(t, &wg, m.Name, 2, http.StatusOK)
 	requireModelReplicas(t, m, 2, "Replicas should be scaled up to 2 to process pending messaging request", autoscaleUpWait)
-	requireModelPods(t, m, 2, "2 Pods should be created for the messaging requests", time.Second)
+	requireModelPods(t, m, 2, "2 Pods should be created for the messaging requests", 5*time.Second)
 	markAllModelPodsReady(t, m)
 
 	// Make sure deployment will not be scaled past max (3).
@@ -96,7 +96,7 @@ vllm:num_requests_waiting{model_name="%s"} %d.0
 	// Ensure the deployment is autoscaled back down to MinReplicas.
 	const autoscaleDownWait = 10 * time.Second
 	requireModelReplicas(t, m, m.Spec.MinReplicas, "Replicas should scale back to MinReplicas", autoscaleDownWait)
-	requireModelPods(t, m, int(m.Spec.MinReplicas), "Pods should be removed", time.Second)
+	requireModelPods(t, m, int(m.Spec.MinReplicas), "Pods should be removed", 5*time.Second)
 
 	t.Log("Waiting for all requests to complete")
 	wg.Wait()
