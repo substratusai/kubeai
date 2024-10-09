73ccfd9ba365bdcbfc1af05d12038ce4191788c6
Nick Stogner <nicholas.stogner@gmail.com>
2024-10-08 21:57:42 -0400

    Update e2e testcase

--- test/e2e/autoscaler-restart/test.sh
+++ test/e2e/autoscaler-restart/test.sh
@@ -17,8 +17,24 @@ trap cleanup EXIT
 
 kubectl apply -f $TEST_DIR/model.yaml
 
-kubectl get -w -o template model/$model --template={{.spec.replicas}} &
+replica_log=$TMP_DIR/model-replicas.log
+kubectl wait --timeout=60s --for=jsonpath='{.spec.replicas}'=3 model/$model
+kubectl get -w -o template model/$model --template='{{ printf "%d\n" .spec.replicas}}' > $replica_log &
 kubectl_watch_pid=$!
 
 sleep 60
+kubectl delete pods -l app.kubernetes.io/name=kubeai
+sleep 120
+
 kill $kubectl_watch_pid
+
+echo "Replica log:"
+cat $replica_log
+replicas_over_time=$(cat $replica_log | sort | uniq)
+
+# Replicas should have remained at 3
+if [ "$replicas_over_time" != "3" ]; then
+    echo "TEST FAILURE: Replicas changed during autoscaler restart."
+    cat $replica_log
+    exit 1
+fi
