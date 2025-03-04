16665002e3a2fccda5f2905ad6f54778bb4f7e4e
Nick Stogner <nicholas.stogner@gmail.com>
2025-03-03 21:01:39 -0500

    Update e2e test

--- test/e2e/model-files/test.sh
+++ test/e2e/model-files/test.sh
@@ -12,13 +12,18 @@ metadata:
   name: ${model_name}
 spec:
   features: [TextGeneration]
-  url: ollama://qwen2:0.5b
-  engine: OLlama
+  url: "hf://facebook/opt-125m"
+  engine: VLLM
   resourceProfile: "cpu:1"
   minReplicas: 1
+  args:
+    # This revision does not contain its own chat template.
+    - --revision=27dcfa74d334bc871f3234de431e71c6eeba5dd6
+  env:
+    VLLM_CPU_KVCACHE_SPACE: "1"
   files:
-    - path: "/config/test-file.txt"
-      content: "test content"
+    - path: "/config/chat-template.jinja"
+      content: "{% for message in messages %}\n{% if message['role'] == 'user' %}\n{{ 'Question:\n' + message['content'] + '\n\n' }}{% elif message['role'] == 'system' %}\n{{ 'System:\n' + message['content'] + '\n\n' }}{% elif message['role'] == 'assistant' %}{{ 'Answer:\n'  + message['content'] + '\n\n' }}{% endif %}\n{% if loop.last and add_generation_prompt %}\n{{ 'Answer:\n' }}{% endif %}{% endfor %}"
     - path: "/config/prompt.txt"
       content: "prompt content"
 EOF
@@ -32,7 +37,24 @@ model_pod=$(kubectl get pods -l "model=${model_name}" -o jsonpath='{.items[0].me
 
 # Check that the files are properly mounted in the model pod
 echo "Checking files are mounted in the model pod..."
-kubectl exec ${model_pod} -- cat /config/test-file.txt | grep "test content"
 kubectl exec ${model_pod} -- cat /config/prompt.txt | grep "prompt content"
+
+# Check that chat completion works.
+curl http://localhost:8000/openai/v1/completions \
+    -H "Content-Type: application/json" \
+    -d '{
+        "model": "files-test-model",
+        "max_tokens": 10,
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": "Write a haiku that explains the concept of recursion."
+            }
+        ]
+    }'
 
 echo "Model files e2e test passed!"
