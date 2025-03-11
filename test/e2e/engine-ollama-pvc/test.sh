e80446c01dcc4b8ac4bb4c84e54d6a08a172a67e
Kyle Sorensen <sorensenkw@gmail.com>
2025-03-10 22:18:56 -0600

    update e2e test

--- test/e2e/engine-ollama-pvc/test.sh
+++ test/e2e/engine-ollama-pvc/test.sh
@@ -11,17 +11,17 @@ mkdir -p ${PV_HOST_PATH}
 # Execute into the kind container - ollama pull
 kind_container=$(docker ps --filter "name=kind-control-plane" --format "{{.ID}}")
 # pull qwen:0.5b model into /usr/share/ollama/.ollama/models/
-docker exec -i $kind_container bash -c "
-  mkdir -p ${PV_HOST_PATH}
-  curl -L https://ollama.com/download/ollama-linux-amd64.tgz -o ollama-linux-amd64.tgz
-  tar -C /usr -xzf ollama-linux-amd64.tgz
-  ollama pull qwen:0.5b"
+#docker exec -i $kind_container bash -c "
+#  mkdir -p ${PV_HOST_PATH}
+#  curl -L https://ollama.com/download/ollama-linux-amd64.tgz -o ollama-linux-amd64.tgz
+#  tar -C /usr -xzf ollama-linux-amd64.tgz
+#  ollama pull qwen:0.5b"
 
 kubectl apply -f $REPO_DIR/test/e2e/engine-ollama-pvc/pv.yaml
 kubectl apply -f $REPO_DIR/test/e2e/engine-ollama-pvc/pvc.yaml
 
 # Apply the Ollama hydrate job
-kubectl apply -f $REPO_DIR/test/e2e/engine-vllm-pvc/ollama-hydrate-job.yaml
+kubectl apply -f $REPO_DIR/test/e2e/engine-ollama-pvc/ollama-hydrate-job.yaml
 
 # Wait for job completion with timeout
 timeout=600 # 10 minutes
@@ -57,7 +57,7 @@ done
 
 helm install $models_release $REPO_DIR/charts/models -f - <<EOF
 catalog:
-  qwen-500m:
+  qwen-500m-cpu:
     enabled: true
     url: pvc://model-pvc?model=qwen:0.5b
     minReplicas: 2
@@ -73,7 +73,7 @@ for i in {1..10}; do
   curl http://localhost:8000/openai/v1/completions \
     --max-time 600 \
     -H "Content-Type: application/json" \
-    -d '{"model": "opt-125m-cpu", "prompt": "Who was the first president of the United States?", "max_tokens": 40}'
+    -d '{"model": "qwen-500m-cpu", "prompt": "Who was the first president of the United States?", "max_tokens": 40}'
 done
 
 sleep 300
