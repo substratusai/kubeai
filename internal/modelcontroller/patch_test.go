24887c0191ce03d81edadb21b0de11a09a00386d
alam0rt <sam@samlockart.com>
2025-04-10 09:53:20 +1000

    test hashes to ensure pod isnt changed

--- internal/modelcontroller/patch_test.go
+++ internal/modelcontroller/patch_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"github.com/substratusai/kubeai/internal/config"
+	"github.com/substratusai/kubeai/internal/k8sutils"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
@@ -13,24 +14,30 @@ import (
 func Test_applyJSONPatchToPod(t *testing.T) {
 	t.Parallel()
 
+	emptyPod := &corev1.Pod{}
+	emptyPodHash := k8sutils.PodHash(emptyPod.Spec)
+
 	cases := map[string]struct {
-		modelPod    *corev1.Pod
-		patches     []config.JSONPatch
-		want        *corev1.Pod
-		errorString *string
+		modelPod     *corev1.Pod
+		patches      []config.JSONPatch
+		want         *corev1.Pod
+		errorString  *string
+		expectedHash *string
 	}{
 		"no-patch": {
-			modelPod: &corev1.Pod{},
-			patches:  nil,
-			want:     &corev1.Pod{},
+			modelPod:     emptyPod,
+			patches:      nil,
+			want:         emptyPod,
+			expectedHash: ptr.To(emptyPodHash), // default hash
 		},
 		"error-patch": {
-			modelPod: &corev1.Pod{},
+			modelPod: emptyPod,
 			patches: []config.JSONPatch{
 				{Op: "invalid", Path: "/spec/containers/0/image", Value: "new-image"},
 			},
-			want:        &corev1.Pod{},
-			errorString: ptr.To("apply pod patch: Unexpected kind: invalid"),
+			want:         emptyPod,
+			expectedHash: ptr.To(emptyPodHash), // since the patch failed it should not change the hash
+			errorString:  ptr.To("apply pod patch: Unexpected kind: invalid"),
 		},
 		"replace-image": {
 			modelPod: &corev1.Pod{
@@ -91,11 +98,17 @@ func Test_applyJSONPatchToPod(t *testing.T) {
 	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
+			gotHash := k8sutils.PodHash(c.modelPod.Spec)
+
 			err := applyJSONPatchToPod(c.patches, c.modelPod)
 			if c.errorString != nil {
 				require.ErrorContains(t, err, *c.errorString)
 			} else {
 				require.NoError(t, err)
+			}
+
+			if c.expectedHash != nil {
+				require.Equal(t, *c.expectedHash, gotHash, "expected pod hash to be %s", *c.expectedHash)
 			}
 
 			require.Equal(t, c.want, c.modelPod, "expected pod to be patched correctly")
